686a519a7b6346ab3e6472e18e6d8e254982bb97
h.matsuo <matsuoshi@gmail.com>
2022-09-19 11:13:12 +0900

    enter and bs test

--- keyboards/amag23/keymaps/left/keymap.c
+++ keyboards/amag23/keymaps/left/keymap.c
@@ -1,17 +1,5 @@
-/* Copyright 2021
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * amag23 Left
  */
 #include QMK_KEYBOARD_H
 
@@ -25,9 +13,9 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-      LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+      KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
       KC_LSFT, LT(FN1,KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,
-      KC_RALT, KC_LALT, KC_LGUI, LGUI_T(KC_LANG2),          KC_SPC
+      RALT_T(KC_ESC), KC_LALT, KC_BSPC, LGUI_T(KC_LANG2),          KC_SPC
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
       KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,
--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -1,17 +1,5 @@
-/* Copyright 2021
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * amag23 Right
  */
 #include QMK_KEYBOARD_H
 
@@ -24,8 +12,8 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
-      KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_BSPC,
-      KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_EQL,
+      KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_MINS,
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, LSFT_T(KC_SLSH),
       RCTL_T(KC_ENT), RGUI_T(KC_LANG1), KC_LEFT, KC_DOWN,  KC_RGHT
   ),
