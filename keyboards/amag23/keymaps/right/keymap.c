15dad17a0b28454f87ef4cc0396ad078f785ee6c
h.matsuo <matsuoshi@gmail.com>
2022-09-19 17:10:38 +0900

    update

--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -12,8 +12,8 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
-      KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_MINS,
-      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_BSPC,
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT,
       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, LSFT_T(KC_SLSH),
       RCTL_T(KC_ENT), RGUI_T(KC_LANG1), KC_LEFT, KC_DOWN,  KC_RGHT
   ),
