8c66203862e908261e78485b9f64fc15f4fbedef
h.matsuo <matsuoshi@gmail.com>
2022-09-19 10:33:53 +0900

    modify right enter

--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -25,9 +25,9 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
       KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_BSPC,
-      KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT,
-      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_LSFT,
-      KC_RCTL, KC_LEFT, KC_DOWN, KC_UP,            KC_RGHT
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_EQL,
+      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, LSFT_T(KC_SLSH),
+      RCTL_T(KC_ENT), RGUI_T(KC_LANG1), KC_LEFT, KC_DOWN,  KC_RGHT
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_0,
