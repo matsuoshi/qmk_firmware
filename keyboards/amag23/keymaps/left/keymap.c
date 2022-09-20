f2331a65f3aa25e6bb63671636b107feef452394
h.matsuo <matsuoshi@gmail.com>
2022-09-20 23:47:14 +0900

    update fn

--- keyboards/amag23/keymaps/left/keymap.c
+++ keyboards/amag23/keymaps/left/keymap.c
@@ -13,15 +13,15 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-      KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-      KC_LSFT, LT(FN1,KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,
-      RALT_T(KC_ESC), KC_LALT, KC_BSPC, LGUI_T(KC_LANG2),          KC_SPC
+      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+      KC_RCTL, KC_LALT, KC_BSPC, KC_LGUI,          KC_SPC
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
-      KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,
-      KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,
-      KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,          KC_0
+      _______, KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,
+      _______, KC_NO,   KC_4,    KC_5,    KC_6,    KC_COLN,
+      _______, KC_NO,   KC_1,    KC_2,    KC_3,    KC_DOT,
+      _______, _______, _______, _______,          KC_0
   ),
 };
 // clang-format on
--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -12,16 +12,16 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
-      KC_Y,    KC_U,    KC_I,    KC_O,    LT(FN1,KC_P),    KC_BSPC,
+      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_H,    KC_J,    KC_K,    KC_L,    KC_MINS, KC_ENT,
-      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, LSFT_T(KC_SLSH),
-      RCTL_T(KC_ENT), RGUI_T(KC_LANG1), KC_LEFT, KC_DOWN,  KC_RGHT
+      KC_N,    KC_M,    KC_COMM, LT(FN1,KC_DOT),  KC_RSFT, KC_UP,
+      KC_ENT,  KC_RGUI, KC_LEFT, KC_RGHT, KC_DOWN
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_0,
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO
+      KC_7,    KC_8,    KC_9,   KC_NO,   KC_NO,   KC_NO,
+      KC_4,    KC_5,    KC_6,   KC_NO,   KC_NO,   KC_NO,
+      KC_1,    KC_2,    KC_3,   KC_NO,   KC_NO,   KC_NO,
+      KC_0,    KC_COLN, KC_NO,  KC_NO,            KC_NO
     ),
 };
 // clang-format on
