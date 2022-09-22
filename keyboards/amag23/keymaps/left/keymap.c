2f5a57fe22cc1dbbc7ca203f2112ad545c9387e2
h.matsuo <matsuoshi@gmail.com>
2022-09-23 08:27:21 +0900

    update

--- keyboards/amag23/keymaps/left/keymap.c
+++ keyboards/amag23/keymaps/left/keymap.c
@@ -12,16 +12,16 @@ enum layer_names {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_all( /* Base Layer */
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-      KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+      KC_LSFT, KC_Z,    LT(FN1,KC_X),    KC_C,    KC_V,    KC_B,
       KC_RCTL, KC_LALT, KC_BSPC, KC_LGUI,          KC_SPC
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
-      _______, KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,
-      _______, KC_NO,   KC_4,    KC_5,    KC_6,    KC_COLN,
-      _______, KC_NO,   KC_1,    KC_2,    KC_3,    KC_DOT,
-      _______, _______, _______, _______,          KC_0
+      _______, KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,
+      _______, KC_NO,   KC_COLN, KC_4,    KC_5,    KC_6,
+      _______, KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,
+      _______, _______, _______, KC_DOT,           KC_0
   ),
 };
 // clang-format on
--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ENT,  KC_RGUI, KC_LEFT, KC_RGHT, KC_DOWN
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
-      KC_7,    KC_8,    KC_9,   KC_NO,   KC_NO,   KC_NO,
-      KC_4,    KC_5,    KC_6,   KC_NO,   KC_NO,   KC_NO,
-      KC_1,    KC_2,    KC_3,   KC_NO,   KC_NO,   KC_NO,
-      KC_0,    KC_COLN, KC_NO,  KC_NO,            KC_NO
+      KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,   KC_NO,
+      KC_4,    KC_5,    KC_6,    KC_COLN, KC_NO,   KC_NO,
+      KC_1,    KC_2,    KC_3,    KC_NO,   KC_NO,   KC_NO,
+      KC_0,    KC_DOT,  KC_SLSH, KC_NO,            KC_NO
     ),
 };
 // clang-format on
