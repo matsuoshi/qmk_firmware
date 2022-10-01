2c5c0fdcdb32fb5655b589d5355f5d5560ebe1dd
h.matsuo <matsuoshi@gmail.com>
2022-10-02 08:02:09 +0900

    modify 10keys

--- keyboards/amag23/keymaps/right/keymap.c
+++ keyboards/amag23/keymaps/right/keymap.c
@@ -19,10 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_RALT, KC_LEFT, KC_DOWN, KC_UP,            KC_RGHT
   ),
   [FN1] = LAYOUT_all( /* Function Layer */
-      KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,
-      KC_MINS, KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,
+      KC_ESC,  KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,
+      KC_TAB,  KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,
       KC_COLN, KC_1,    KC_2,    KC_3,    KC_NO,   KC_NO,
-      KC_NO,   KC_0,    KC_DOT,  KC_SLSH,          KC_NO
+      KC_ENT,  KC_0,    KC_COMM, KC_DOT,          KC_NO
   ),
   [FN2] = LAYOUT_all( /* Function Layer */
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NO,   KC_NO,
